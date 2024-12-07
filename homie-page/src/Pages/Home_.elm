4720d84552018a9b59ce88cef853a0c8c2506c38
JulieGunawan <julie.gunawan@protonmail.com>
2024-12-07 10:55:10 -0700

    Not working call

--- homie-page/src/Pages/Home_.elm
+++ homie-page/src/Pages/Home_.elm
@@ -2,8 +2,15 @@ module Pages.Home_ exposing (page)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http
 import View exposing (View)
 import Components.Sidebar
+import Json.Decode as Decode
+import Array exposing (initialize)
+import Components.Sidebar exposing (view)
+import Dict exposing (update)
+
 
 page : View msg
 page =
@@ -22,7 +29,45 @@ page =
                                 ) is a micro-cap regional food and dessert supply with an emphasis on servicing pets, especially cats and dogs
                                 """
                         ]
+                        , div [] 
+                        [
+                            h3[][text "Wanna see some cats? Click this button to get different cats"]
+                            , button[] [text "Click Me"]
+                        ]
                     ]
                 ]
             ] 
         }
+
+main: Program Never Model Msg
+main =
+    Html.program
+    {
+        init = init
+        , view = view
+        , update =update
+        , subcriptions = \_ -> Sub.none
+    }
+
+initialModel : Model
+initialModel = 
+    { gifUrl = "waiting.gif"
+    , error= Nothing
+    }
+
+init: (Model, Cmd Msg)
+init = 
+    (initialModel, getRandomCatGif)
+
+
+apiUrl : String
+apiUrl = "https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1"
+
+getRandomCatGif: Cmd Msg
+getRandomCatGif = 
+    Http.get apiUrl decodeGifUrl
+        |> Http.send NewGif
+
+decodeGifUrl: Decode.Decoder String
+decodeGifUrl =
+    Decode.at ["data", "image_url"] Decode.string
